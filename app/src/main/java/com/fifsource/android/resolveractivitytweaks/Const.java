01e88f138b0c6f7e13af3ee926a3d17121de4f96
Philippe Troin <phil+github-commits@fifi.org>
2019-02-26 00:49:31 -0800

    Add license and copyrights in source.

--- app/src/main/java/com/fifsource/android/resolveractivitytweaks/Const.java
+++ app/src/main/java/com/fifsource/android/resolveractivitytweaks/Const.java
@@ -1,8 +1,22 @@
+// Resolver Activity Tweaks - Xposed module to tweak the Android
+// Resolver Activity
+// Copyright (C) 2015-2019 Philippe Troin (F-i-f on Github)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package com.fifsource.android.resolveractivitytweaks;
 
-/**
- * Created by phil on 10/1/15.
- */
 class Const {
     @SuppressWarnings("SpellCheckingInspection")
     public static final String[] XPOSED_INSTALLER_PACKAGE_NAMES = new String[] {
