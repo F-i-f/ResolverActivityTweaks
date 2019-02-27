53fd74a5383f9bf9c3746fadc00259c60f7e3206
Philippe Troin <phil+github-commits@fifi.org>
2019-02-26 18:01:47 -0800

    Pacify lint by using package-private access.

--- app/src/main/java/com/fifsource/android/resolveractivitytweaks/Const.java
+++ app/src/main/java/com/fifsource/android/resolveractivitytweaks/Const.java
@@ -19,30 +19,29 @@ package com.fifsource.android.resolveractivitytweaks;
 
 class Const {
     @SuppressWarnings("SpellCheckingInspection")
-    public static final String[] XPOSED_INSTALLER_PACKAGE_NAMES = new String[] {
+    static final String[] XPOSED_INSTALLER_PACKAGE_NAMES = new String[] {
             "com.solohsu.android.edxp.manager",
             "de.robv.android.xposed.installer"
     };
-    public static final String XPOSED_INSTALLER_OPEN_SECTION = ".OPEN_SECTION";
+    static final String XPOSED_INSTALLER_OPEN_SECTION = ".OPEN_SECTION";
 
-    public static final String  PREFERENCES_NAME = "prefs";
-    public static final String  REMOTE_PREFERENCE_AUTHORITY = BuildConfig.APPLICATION_ID + ".preferences";
-    public static final String  PREF_RAT_COPYRIGHT = "rat_copyright";
-    public static final String  PREF_RAT_CATEGORY_NO_XPOSED = "rat_category_no_xposed";
-    public static final String  PREF_RAT_NO_XPOSED_UNINSTALL = "rat_no_xposed_uninstall";
-    public static final String  PREF_RAT_CATEGORY_XPOSED_INACTIVE = "rat_category_xposed_inactive";
-    public static final String  PREF_RAT_XPOSED_INACTIVE_ACTIVATE = "rat_xposed_inactive_activate";
-    public static final String  PREF_RAT_CATEGORY_XPOSED_MISMATCH = "rat_category_xposed_mismatch";
-    public static final String  PREF_RAT_CATEGORY_XPOSED = "rat_category_xposed";
-    public static final String  PREF_RAT_ENABLE = "rat_enable";
-    public static final boolean PREF_RAT_ENABLE_DEFAULT = false;
-    public static final String  PREF_RAT_HIDE_ONCE_ALWAYS = "rat_hideOnceAlways";
-    public static final boolean PREF_RAT_HIDE_ONCE_ALWAYS_DEFAULT = false;
-    public static final String  PREF_RAT_CATEGORY_LAUNCHER = "rat_category_launcher";
-    public static final String  PREF_RAT_SHOW_LAUNCHER_ICON = "rat_showLauncherIcon";
-    public static final boolean PREF_RAT_SHOW_LAUNCHER_ICON_DEFAULT = true;
-    public static final String  PREF_RAT_BUILD_CODE = "rat_build_code";
-    public static final String  PREF_RAT_GIT_REVISION = "rat_git_revision";
-    public static final String  PREF_RAT_LICENSE = "rat_license";
-
+    static final String  PREFERENCES_NAME = "prefs";
+    static final String  REMOTE_PREFERENCE_AUTHORITY = BuildConfig.APPLICATION_ID + ".preferences";
+    static final String  PREF_RAT_COPYRIGHT = "rat_copyright";
+    static final String  PREF_RAT_CATEGORY_NO_XPOSED = "rat_category_no_xposed";
+    static final String  PREF_RAT_NO_XPOSED_UNINSTALL = "rat_no_xposed_uninstall";
+    static final String  PREF_RAT_CATEGORY_XPOSED_INACTIVE = "rat_category_xposed_inactive";
+    static final String  PREF_RAT_XPOSED_INACTIVE_ACTIVATE = "rat_xposed_inactive_activate";
+    static final String  PREF_RAT_CATEGORY_XPOSED_MISMATCH = "rat_category_xposed_mismatch";
+    static final String  PREF_RAT_CATEGORY_XPOSED = "rat_category_xposed";
+    static final String  PREF_RAT_ENABLE = "rat_enable";
+    static final boolean PREF_RAT_ENABLE_DEFAULT = false;
+    static final String  PREF_RAT_HIDE_ONCE_ALWAYS = "rat_hideOnceAlways";
+    static final boolean PREF_RAT_HIDE_ONCE_ALWAYS_DEFAULT = false;
+    static final String  PREF_RAT_CATEGORY_LAUNCHER = "rat_category_launcher";
+    static final String  PREF_RAT_SHOW_LAUNCHER_ICON = "rat_showLauncherIcon";
+    static final boolean PREF_RAT_SHOW_LAUNCHER_ICON_DEFAULT = true;
+    static final String  PREF_RAT_BUILD_CODE = "rat_build_code";
+    static final String  PREF_RAT_GIT_REVISION = "rat_git_revision";
+    static final String  PREF_RAT_LICENSE = "rat_license";
 }
